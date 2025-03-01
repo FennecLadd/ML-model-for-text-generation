b18cdb51cd924306d1ed2cee8fd92d08ac06ba5f
Kushagra <kushagrabhagoliwal1311@gmail.com>
2025-03-02 00:13:56 +0530

    Update mltextdevmodel.py

--- mltextdevmodel.py
+++ mltextdevmodel.py
@@ -1,1 +1,143 @@
 # ML-model-for-text-generation
+#%tensorflow_version 2.x
+from keras.preprocessing import sequence
+import keras
+import tensorflow as tf
+import os
+import numpy as np
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+vocab = sorted(set(text))
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+def text_to_int(text):
+  return np.array([char2idx[c] for c in text])
+text_as_int = text_to_int(text)
+
+#func to create num value to text
+def int_to_text(ints):
+  try:
+    ints = ints.numpy()
+  except:
+    pass
+  return ''.join(idx2char[ints])
+print(int_to_text(text_as_int[:13]))
+
+seq_length = 100  # length of sequence for a training example
+examples_per_epoch = len(text)//(seq_length+1)
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+def split_input_target(chunk):  # for the example: hello
+    input_text = chunk[:-1]  # hell
+    target_text = chunk[1:]  # ello
+    return input_text, target_text  # hell, ello
+
+dataset = sequences.map(split_input_target)
+for x, y in dataset.take(2):
+  print("\n\nEXAMPLE\n")
+  print("INPUT")
+  print(int_to_text(x))
+  print("\nOUTPUT")
+  print(int_to_text(y))
+
+BATCH_SIZE = 64
+VOCAB_SIZE = len(idx2char)  # vocab is number of unique characters
+EMBEDDING_DIM = 256
+RNN_UNITS = 1024
+# Buffer size to shuffle the dataset
+BUFFER_SIZE = 10000
+data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=None),  # Remove batch_input_shape
+        tf.keras.layers.LSTM(rnn_units,
+                             return_sequences=True,
+                             stateful=True,
+                             recurrent_initializer='glorot_uniform'),
+        tf.keras.layers.Dense(vocab_size)
+    ])
+    return model
+# Define the hyperparameters
+VOCAB_SIZE = 1000
+EMBEDDING_DIM = 256
+RNN_UNITS = 1024
+BATCH_SIZE = 64
+
+model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
+model.summary()
+
+def int_to_text(ints):
+    return ''.join(idx2char[i] for i in ints if i < len(idx2char))  # Avoid index out of bounds
+
+for input_example_batch, target_example_batch in data.take(1):
+  example_batch_predictions = model(input_example_batch)  # ask our model for a prediction on our first batch of training data (64 entries)
+  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+pred = example_batch_predictions[0]
+time_pred = pred[0]
+sampled_indices = tf.random.categorical(pred, num_samples=1)
+sampled_indices = tf.clip_by_value(sampled_indices, 0, VOCAB_SIZE - 1)  # Fix out-of-range error
+sampled_indices = np.reshape(sampled_indices, (1, -1))[0]
+predicted_chars = int_to_text(sampled_indices)
+
+predicted_chars
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+model.compile(optimizer='adam', loss=loss)
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}.weights.h5")  # Corrected file extension
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix, save_weights_only=True)
+
+history = model.fit(data, epochs=10, callbacks=[checkpoint_callback])
+model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, batch_size=1)
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+checkpoint_num = 10
+model.load_weights(tf.train.load_checkpoint("./training_checkpoints/ckpt_" + str(checkpoint_num)))
+model.build(tf.TensorShape([1, None]))
+
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 800
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+
+      predictions = tf.squeeze(predictions, 0)
+
+      # using a categorical distribution to predict the character returned by the model
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+      # We pass the predicted character as the next input to the model
+      # along with the previous hidden state
+      input_eval = tf.expand_dims([predicted_id], 0)
+
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+inp = input("Type a starting string: ")
+print(generate_text(model, inp))
